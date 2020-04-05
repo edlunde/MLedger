e983c5b81ec813ad9c476b988d4b03986b22f87a
Erik Edlund <edlunde@gmail.com>
2020-04-05 20:44:27 +0200

    Added categorizationFormRow

--- MLedger.m
+++ MLedger.m
@@ -422,6 +422,21 @@ sortByDateDescending[list_] :=
 
 (* ::Subsection::Closed:: *)
 (*Categorization form*)
+
+
+(* Note: not tested for Dynamic category-variable functionality 
+   (most important part of course, but requires some arcane workaround to get right
+   behavior when run without notebook?) *)
+categorizationFormRow[entry_?IsJournalEntry] :=
+ With[{category = Unique@"category", fieldSize = 11},
+  Join[
+   Values[entry[[{"date", "description", "amount"}]]], 
+   {
+    PopupMenu[Dynamic[category], getCategories[entry], FieldSize -> fieldSize],
+    InputField[Dynamic[category], String, FieldSize -> fieldSize]
+   }
+  ]
+ ]
 (* ::Section::Closed:: *)
 (*Tail*)
 End[];
